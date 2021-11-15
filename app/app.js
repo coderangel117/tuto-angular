14ae6e54f312cfa8000066b48b68558569980383
Gabriel PERINO <perinogabriel1@gmail.com>
2021-11-15 10:58:47 +0100

    app js files

---
+++ app/app.js
@@ -0,0 +1,19 @@
+
+// Define the `phonecatApp` module
+var phoneApp = angular.module('phoneApp', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phoneApp.controller('PhoneListController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
